4debd71fced35551aafed811a77fc2169bac3ba0
Gdek <alex967123@gmail.com>
2015-03-31 11:30:42 -0400

    Update

--- app.js
+++ app.js
@@ -1,69 +1,95 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express    = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/water');
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var schoolSchema = new Schema({
+  school: String,
+  latitude: Number,
+  longitude: Number
+});
+
+var pledgeSchema = new Schema({
+  name: String,
+  initial: String,
+  pledge: String,
+  date: { type: Date, default: Date.now }
+});
+
+var School = mongoose.model('School', schoolSchema);
+var Pledge = mongoose.model('Pledge', pledgeSchema);
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Mongo connected');
+  parseSchools();
+  //var pledgeexample = new Pledge({ name: 'Alex', initial: 'J', pledge: 'gardener' });
+  //pledgeexample.save();
+  //console.log(pledgeexample._id);
+  //var result = Pledge.find( function(err, pledges) {
+  //  if (err) return console.error(err);
+  //  console.log(pledges);
+  //});
+});
 
 var app = express();
 
- //view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+// parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-app.post('/acceptContacts', function(req, res) {
-  console.log(req.body);
-  console.log(req.body.hello);
-  res.send(200);
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(function (req, res, next) {
+  console.log(req.body); // populated!
+  next()
 });
 
 app.listen(8080);
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.post('/', function(req, res){
+  console.log(req.body);      // your JSON
+  addPledge(req.body);
+  res.send(req.body);    // echo the result back
 });
 
+function addPledge(pledge) {
+  //Seperate pledge parameters
+  //Add pledge to database
+  var pledgeObj = JSON.parse(pledge);
+  var wstream = fs.createWriteStream('pledges.txt');
+  wstream.write(pledge);
+  wstream.end();
+}
 
-module.exports = app;
+function parseSchools() {
+  fs.readFile("schools.json", 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var schools = JSON.parse(data);
+    for (var i=0;i<schools.SchoolList.length;i++) {
+      var schoolObj = schools.SchoolList[i];
+      var school = new School({ school: schoolObj.Name, latitude: schoolObj.Lat, longitude: schoolObj.Long });
+      console.log(school._id);
+      school.save();
+      var result = School.find( function(err, schoolses) {
+         if (err) return console.error(err);
+          console.log(schoolses);
+        });
+    }
+  });
+}
+
+//construct school list
+//
+
+function addSchool(school) {
+
+}
+
+//New function create pledgemap
+//Get pledge data
