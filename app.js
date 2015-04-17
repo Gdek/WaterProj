a6ed4b9dea83ef7542ab303e2c4bfc87d3accef4
Gdek <alex967123@gmail.com>
2015-04-17 17:46:13 -0400

    Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/KeikiCorner');
+mongoose.connect('mongodb://localhost/KeikiCornerPledges');
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
